398cd00744c5b256f8d9854da0eff6c85cf7b42e
Leslie346 <lesliecruz346@gmail.com>
2022-05-06 16:48:38 -0500

    make it better

--- routes/ejercicios.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-let ejerciciosController = require('../controllers/ejerciciosController');
-
-let router = express.Router();
-
-router.get('/:idEjercicio', ejerciciosController.detalle);
-
-module.exports = router;
