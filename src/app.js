ca11e8091e243c6a340fcf9ee3a03260273d609a
Leslie346 <lesliecruz346@gmail.com>
2022-07-11 01:36:04 -0500

    cambios en app

--- src/app.js
+++ src/app.js
@@ -35,13 +35,13 @@ app.use(userLoggedMiddleware);
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, './views'));
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const publicFolderPath = path.resolve(__dirname, '/public');
+const publicFolderPath = path.resolve(__dirname, './public');
 app.use(express.static(publicFolderPath));
 
 let port = process.env.PORT || 3000;
